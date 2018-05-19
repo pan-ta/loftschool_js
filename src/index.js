ee43e230c2b905ac00705b0683cfb74277c87613
Tatiana Shimbereva <pan-ta@yandex.ru>
2018-05-20 01:50:34 +0300

    дз5, первая часть

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@
  Функция должна добавлять обработчик fn события eventName к элементу target
 
  Пример:
-   addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
+   addListener('click', document.querySelector('a'), () => console.log('...')) 
+   // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+    target.addEventListener(eventName, fn); 
 }
 
 /*
@@ -17,9 +19,11 @@ function addListener(eventName, target, fn) {
  Функция должна удалять у элемента target обработчик fn события eventName
 
  Пример:
-   removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
+   removeListener('click', document.querySelector('a'), someHandler) 
+   // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+    target.removeEventListener(eventName, fn);
 }
 
 /*
@@ -28,9 +32,13 @@ function removeListener(eventName, target, fn) {
  Функция должна добавить к элементу target такой обработчик на события eventName, чтобы он отменял действия по умолчанию
 
  Пример:
-   skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
+   skipDefault('click', document.querySelector('a')) 
+   // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+    target.addEventListener(eventName, function(e) {
+        e.preventDefault();
+    })
 }
 
 /*
@@ -42,6 +50,10 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+    // target.emulateClick();
+    var event = new Event('click');
+
+    target.dispatchEvent(event);
 }
 
 /*
@@ -51,9 +63,16 @@ function emulateClick(target) {
  который реагирует (вызывает fn) только на клики по элементам BUTTON внутри target
 
  Пример:
-   delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
+   delegate(document.body, () => console.log('кликнули на button')) 
+   // добавит такой обработчик кликов для body, который будет вызывать указанную функцию 
+   только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+    for (const element of target.children) {
+        if (element.tagName === 'BUTTON') {
+            element.addEventListener ('click', fn);
+        }
+    }
 }
 
 /*
@@ -63,9 +82,16 @@ function delegate(target, fn) {
  который сработает только один раз и удалится (перестанет срабатывать для последующих кликов по указанному элементу)
 
  Пример:
-   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
+   once(document.querySelector('button'), () => console.log('обработчик выполнился!')) 
+   // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+    function playAndRemove() {
+        fn();
+        target.removeEventListener('click', playAndRemove);
+    }
+
+    target.addEventListener('click', playAndRemove);
 }
 
 export {
