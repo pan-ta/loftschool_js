9b4b7e1d5a13730d4332b221dd11b51852283ae2
Tatiana Shimbereva <pan-ta@yandex.ru>
2018-06-01 15:55:15 +0300

    добавление и фильтрация через map - работают. добавление в браузер - не работает:( html прилагаю

--- src/cookie.js
+++ src/cookie.js
@@ -43,10 +43,141 @@ const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
 
-filterNameInput.addEventListener('keyup', function() {
-    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
-});
+// filterNameInput.addEventListener('keyup', function() {
+//     // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+// });
+
+// addButton.addEventListener('click', () => {
+//     // здесь можно обработать нажатие на кнопку "добавить cookie"
+// });
+
+const cookieMap = new Map();
+const emulKeyup = new Event("keyup");
+
+function isMatching(full, chunk) {
+
+    const fullUpperCase = full.toUpperCase();
+    const chunkUpperCase = chunk.toUpperCase();
+
+    const fullArr = fullUpperCase.split('');
+    const chunkArr = chunkUpperCase.split('');
+
+    let isAny = false;
+    let isMatching = true;
+    let j;
+
+    for (j = 0; j < fullArr.length; j++) {
+        if (fullArr[j] === chunkArr[0]) {
+            isAny = true;
+            break;
+        }
+    }
+        
+    if (isAny == true) {
+        for (let i = 0; i < chunkArr.length; i++) {
+            if (chunkArr[i] != fullArr[j]) {
+                isMatching = false;
+                break;
+            } else {
+                j++;
+            }
+        }
+
+    } else {
+        isMatching = false;
+    }
+
+    return isMatching;
+}
+
+function createRow(name, value) {
+    const newTableRow = document.createElement("TR");
+    newTableRow.classList.add("list-table__row");
+    listTable.appendChild(newTableRow);
+
+    const newNameCell = document.createElement("TH");
+    newNameCell.classList.add("list-table__name-cell");
+    newNameCell.textContent = name;
+    newTableRow.appendChild(newNameCell);
+
+    const newValueCell = document.createElement("TH");
+    newValueCell.classList.add("list-table__value-cell");
+    newValueCell.textContent = value;
+    newTableRow.appendChild(newValueCell);
+
+    const newButtonCell = document.createElement("TH");
+    newNameCell.classList.add("list-table__button-cell");
+    newTableRow.appendChild(newButtonCell);
+
+    const deleteCookieButton = document.createElement("BUTTON");
+    deleteCookieButton.classList.add("list-table__delete-button");
+    deleteCookieButton.textContent = "Удалить";
+    newButtonCell.appendChild(deleteCookieButton);
+
+    deleteCookieButton.addEventListener("click", () => {
+        newTableRow.remove();
+        cookieMap.delete(name);
+        console.log(cookieMap);
+
+        document.cookie = `"${name}=; expires=-1"` 
+
+    })
+}
+
 
 addButton.addEventListener('click', () => {
-    // здесь можно обработать нажатие на кнопку "добавить cookie"
+    if (addNameInput.value && addValueInput.value) {
+
+        if (cookieMap.has(addNameInput.value)) {
+            alert("Значение куки будет перезаписано");
+            cookieMap.delete(addNameInput.value);
+        }
+
+        cookieMap.set(addNameInput.value, addValueInput.value);
+        console.log(cookieMap);
+
+        document.cookie = `"${addNameInput.value}=${addValueInput.value}"`;
+        console.log("сейчас в браузере эти куки: ", document.cookie); 
+
+        filterNameInput.dispatchEvent(emulKeyup);
+
+        addNameInput.value = "";
+        addValueInput.value = "";
+
+    } else {
+        alert("Введите имя и значение новой куки");
+    }
+    
 });
+
+
+filterNameInput.addEventListener('keyup', function() {
+    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+
+    const inputChunk = filterNameInput.value;
+
+    if (inputChunk) {
+    
+        while (listTable.lastChild) {
+            listTable.lastChild.remove();
+        }
+
+        for (const cookie of cookieMap) {
+            if (isMatching(cookie[0], inputChunk) === true || isMatching(cookie[1], inputChunk) === true) {
+                console.log("совпало", cookie, inputChunk);
+                createRow(cookie[0], cookie[1]);
+            }
+        }
+
+    } else {
+
+        while (listTable.lastChild) {
+            listTable.lastChild.remove();
+        }
+
+        console.log(cookieMap);
+        for (const cookie of cookieMap) {
+            createRow(cookie[0], cookie[1]);
+        }
+    }
+})
